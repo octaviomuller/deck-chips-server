dbdb19a848e1abc1913367800fd0fbdb9d4b975a
octaviomuller <octavio.muller@bleu.com.br>
2022-06-08 22:45:26 -0300

    teste

--- config/database.go
+++ config/database.go
@@ -2,20 +2,14 @@ package config
 
 import (
 	"context"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/octaviomuller/deck-chips-server/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDatabase() *mongo.Database {
-	if err := godotenv.Load("../.env"); err != nil {
-		log.Fatal("No .env file found")
-	}
-
 	uri := os.Getenv("MONGO_URI")
 	if uri == "" {
 		panic("You must set your 'MONGODB_URI' environmental variable.")
